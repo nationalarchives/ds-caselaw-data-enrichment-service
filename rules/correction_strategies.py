b9af114e658bc220bd387e2948f15552f1327adc
Daniel Hoadley <dan.hoadley@gmail.com>
2022-02-25 14:25:42 +0000

    Update correction_strategies.py
    Added remaining strategies

--- rules/correction_strategies.py
+++ rules/correction_strategies.py
@@ -38,4 +38,52 @@ def apply_correction_strategy(citation_type, citation_match, canonical_form):
         second_num = components[2]
         corrected_citation = canonical_form.replace("dddd", year).replace("d1", first_num).replace("d2", second_num)
 
-    return corrected_citation
+    # e.g. Case C-123/12
+    elif citation_type == "EUCCase":
+        components = re.findall(r'\d+', citation_match)
+        year = "No year"
+        first_num = components[0]
+        second_num = components[1]
+        corrected_citation = canonical_form.replace("d1", first_num).replace("d2", second_num)
+
+    # e.g. Case T-123/12
+    elif citation_type == "EUTCase":
+        components = re.findall(r'\d+', citation_match)
+        year = "No year"
+        first_num = components[0]
+        second_num = components[1]
+        corrected_citation = canonical_form.replace("d1", first_num).replace("d2", second_num)
+        
+    # e.g. [2022] 1 WLR 123
+    elif citation_type == "PubYearNumAbbrNum":
+        components = re.findall(r'\d+', citation_match)
+        year = components[0]
+        first_num = components[1]
+        second_num = components[2]
+        corrected_citation = canonical_form.replace("dddd", year).replace("d1", first_num).replace("d2", second_num)
+
+    # e.g. [2022] QB 123
+    elif citation_type == "PubYearAbbrNum":
+        components = re.findall(r'\d+', citation_match)
+        year = components[0]
+        num = components[1]
+        corrected_citation = canonical_form.replace("dddd", year).replace("d+", num)
+
+    # 1 ExD 123
+    elif citation_type == "PubNumAbbrNum":
+        components = re.findall(r'\d+', citation_match)
+        year = "No year"
+        first_num = components[0]
+        second_num = components[1]
+        corrected_citation = canonical_form.replace("d1", first_num).replace("d2", second_num)
+
+    # LR 1 ScDiv 123
+    elif citation_type == "PubAbbrNumAbbrNum":
+        components = re.findall(r'\d+', citation_match)
+        year = "No year"
+        first_num = components[0]
+        second_num = components[1]
+        corrected_citation = canonical_form.replace("d1", first_num).replace("d2", second_num)
+
+    return (corrected_citation, year)
+
