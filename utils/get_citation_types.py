7c31a066f6d5762dd468a95df800d24ce977475a
Amy Conroy <amyeileenconroy@gmail.com>
2022-04-07 11:43:40 +0100

    file restructuring

--- utils/get_citation_types.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-MANIFEST = pd.read_csv("/Users/daniel.hoadley/Development/project_TNA/rules/2022_02_24_Citation_Manifest.csv")
-citation_types = list(set(MANIFEST["citationType"].tolist()))
-print (citation_types)
