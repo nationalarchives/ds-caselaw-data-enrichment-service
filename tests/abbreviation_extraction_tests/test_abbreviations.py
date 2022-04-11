28e174a1f8fddfe2b8f25c8baf0d5a7c434dfbe6
Amy Conroy <amyeileenconroy@gmail.com>
2022-04-11 16:43:54 +0100

    testing quotation types

--- tests/abbreviation_extraction_tests/test_abbreviations.py
+++ tests/abbreviation_extraction_tests/test_abbreviations.py
@@ -1,7 +1,9 @@
 import unittest
 import sys
+
+from numpy import short
 sys.path.append("./")
-from abbreviation_extraction.abbreviations import AbbreviationDetector, find_abbreviation
+from abbreviation_extraction.abbreviations import AbbreviationDetector, find_abbreviation, filter_matches
 import spacy 
 from spacy.language import Language
 from spacy.tokens import Span, Doc
@@ -34,12 +36,72 @@ class TestAbbrevationMatcher(unittest.TestCase):
         _, long_form = find_abbreviation(long, short)
         assert long_form.text == "Companies Act 2006"
 
-        text = "The provision is found in the Companies Act 2006 (\"CA 2006\")"
+        text = "Upper Tribunal (\"UT\") "
         doc = self.nlp(text)
-        long = doc[6:9]
-        short = doc[10:14]
+        long = doc[0:2]
+        short = doc[3:5]
         _, long_form = find_abbreviation(long, short)
-        assert long_form.text == "Companies Act 2006"
+        assert long_form.text == "Upper Tribunal"
+
+        text = "section 17 of the Value Added Tax Act (\"the VAT Act\") "
+        doc = self.nlp(text)
+        long = doc[4:8]
+        short = doc[9:13]
+        _, long_form = find_abbreviation(long, short)
+        assert long_form.text == "Value Added Tax Act"
+    
+    def test_short_form(self): 
+        # test that the short forms are being parsed and found correctly by filter_matches
+        text = "Companies Act 2006 (\"CA 2006\")"
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 4, 8)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "CA 2006"
+
+        text = "European Economic Area (\"EEA\")"
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 4, 7)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "EEA"
+
+        text = "Value Added Tax Act (\"the VAT Act\") "
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 5, 10)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "the VAT Act"
+
+    # testing that different quotations are supported and the short forms are identified 
+    def test_quotations(self): 
+        # single quotes
+        text = "Value Added Tax Act (\'the VAT Act\') "
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 5, 10)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "the VAT Act"
+
+        # double quotes
+        text = "European Economic Area (\"EEA\")"
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 4, 7)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "EEA"
+
+        text = "European Economic Area (‘EEA‘)"
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 4, 7)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "EEA"
+
+
+        text = "European Economic Area (“EEA“)"
+        doc = self.nlp(text)
+        filtered = filter_matches([(1, 4, 7)], doc)
+        short_form_candidate = filtered[0][1]
+        assert short_form_candidate.text == "EEA"
+
+    # testing that different abbreviations without quotations in their brackets are not parsed correctly
+    def test_no_quotations(self):
+
 
     
 if __name__ == '__main__':
