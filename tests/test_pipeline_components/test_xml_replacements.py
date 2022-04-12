4b689976b14575a58a0bb8bb116cd0c95df83834
Amy Conroy <amyeileenconroy@gmail.com>
2022-04-12 15:10:05 +0100

    tests for each of the files

--- tests/test_pipeline_components/test_xml_replacements.py
+++ tests/test_pipeline_components/test_xml_replacements.py
@@ -17,22 +17,23 @@ def mock_replace_data(file_data):
     tuple_file = open("./tests/test_pipeline_components/verified_test_files/tuples.jsonl", "r")
     for line in tuple_file:
         replacements.append(json.loads(line))
+    
+    tuple_file.close()
 
     replacement_tuples_case = mock_replacement_tuple(replacements, 'case')
     file_data_enriched = replacer_pipeline(file_data, replacement_tuples_case, [], [])
-    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_replaced".replace(".xml", "_enriched.xml")
+    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_replaced.xml".replace(".xml", "_enriched.xml")
     mock_write_file(output_file, file_data_enriched)
 
     replacement_tuples_leg = mock_replacement_tuple(replacements, 'leg')
     file_data_enriched = replacer_pipeline(file_data, replacement_tuples_case, replacement_tuples_leg, [])
-    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_legislation_replaced".replace(".xml", "_enriched.xml")
+    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_legislation_replaced.xml".replace(".xml", "_enriched.xml")
     mock_write_file(output_file, file_data_enriched)
 
     replacement_tuples_abb = mock_replacement_tuple(replacements, 'abb')
     file_data_enriched = replacer_pipeline(file_data, replacement_tuples_case, replacement_tuples_leg, replacement_tuples_abb)
-    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_legislation_abbreviations_replaced".replace(".xml", "_enriched.xml")
+    output_file = f"./tests/test_pipeline_components/output_test_files/case_law_legislation_abbreviations_replaced.xml".replace(".xml", "_enriched.xml")
     mock_write_file(output_file, file_data_enriched)
-
 
 
 def mock_write_file(file_path, file_data_enriched): 
@@ -51,10 +52,38 @@ def mock_replacement_tuple(replacements, key_str):
 
 
 class TestXMLFiles(unittest.TestCase): 
-    def test_case_law(self): 
+    def setUp(self):
         with open("./tests/test_pipeline_components/verified_test_files/test_judgment.xml", "r", encoding="utf-8") as file_in:
             file_data = file_in.read()
             mock_replace_data(file_data)
+
+    def test_case_law(self): 
+        with open("./tests/test_pipeline_components/verified_test_files/test_judgment_caselaw_enriched.xml", "r", encoding="utf-8") as file_in:
+            verified_file_data = file_in.read()
+
+        with open("./tests/test_pipeline_components/output_test_files/case_law_replaced_enriched.xml", "r", encoding="utf-8") as file_in:
+            test_file_data = file_in.read()
+        
+        assert verified_file_data == test_file_data
+    
+    def test_legislation(self): 
+        with open("./tests/test_pipeline_components/verified_test_files/test_judgment_case_and_leg_enriched.xml", "r", encoding="utf-8") as file_in:
+            verified_file_data = file_in.read()
+
+        with open("./tests/test_pipeline_components/output_test_files/case_law_legislation_replaced_enriched.xml", "r", encoding="utf-8") as file_in:
+            test_file_data = file_in.read()
+        
+        assert verified_file_data == test_file_data
+    
+        
+    def test_abbreviations(self): 
+        with open("./tests/test_pipeline_components/verified_test_files/test_judgment_case_and_leg_and_abb_enriched.xml", "r", encoding="utf-8") as file_in:
+            verified_file_data = file_in.read()
+
+        with open("./tests/test_pipeline_components/output_test_files/case_law_legislation_abbreviations_replaced_enriched.xml", "r", encoding="utf-8") as file_in:
+            test_file_data = file_in.read()
+        
+        assert verified_file_data == test_file_data
     
 if __name__ == '__main__':
     unittest.main()
