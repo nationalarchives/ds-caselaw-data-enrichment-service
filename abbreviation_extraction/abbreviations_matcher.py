6bfd279220f72feefc643305ded0d994c368351d
Amy Conroy <amyeileenconroy@gmail.com>
2022-04-12 17:21:58 +0100

    swap case components for abb components

--- abbreviation_extraction/abbreviations_matcher.py
+++ abbreviation_extraction/abbreviations_matcher.py
@@ -1,22 +1,8 @@
-import spacy
-from abbreviation_extraction.abbreviations import AbbreviationDetector
-from spacy.language import Language
 from collections import namedtuple
 
 abb = namedtuple('abb', 'abb_match longform')
 
-def abb_pipeline(judgment_content_text):
-    nlp = spacy.load("en_core_web_sm", exclude=['tok2vec', 'attribute_ruler', 'lemmatizer', 'ner'])
-
-    # init the class - stateful pipeline component 
-    @Language.factory("abbreviation_detector")
-    def create_abbreviation_detector(nlp, name: str): 
-        return AbbreviationDetector(nlp)
-
-    nlp.add_pipe("abbreviation_detector") 
-
-    doc = nlp(judgment_content_text)
-
+def abb_pipeline(doc):
     REPLACEMENTS_ABBR = []
     for abrv in doc._.abbreviations:
         abr_tuple = abb(str(abrv), str(abrv._.long_form))
