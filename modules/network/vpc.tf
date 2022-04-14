63414f72916372b3a9dc837e448a550cb9bb2ae5
niall.roche <niall.roche@gmail.com>
2022-04-14 15:57:01 +0100

    temp remove vpc details from secrets again

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -38,10 +38,10 @@ data "aws_subnets" "private" {
   }
 }
 
-data "aws_subnet" "private" {
-  for_each = toset(data.aws_subnets.private.ids)
-  id       = each.value
-}
+# data "aws_subnet" "private" {
+#   for_each = toset(data.aws_subnets.private.ids)
+#   id       = each.value
+# }
 
 resource "aws_vpc_endpoint" "secrets_manager" {
   vpc_id            = module.vpc.vpc_id
