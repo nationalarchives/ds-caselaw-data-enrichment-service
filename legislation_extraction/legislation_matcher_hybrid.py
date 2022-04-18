4e38828a2ca980e4b3e86a6570c7e07a2c34ca0f
enemsic <94066698+enemsic@users.noreply.github.com>
2022-04-18 22:13:58 +0100

    Testing exact matcher in legislation extraction

--- legislation_extraction/legislation_matcher_hybrid.py
+++ legislation_extraction/legislation_matcher_hybrid.py
@@ -129,7 +129,8 @@ def leg_pipeline(leg_titles, nlp, doc, conn):
     shorttitles = leg_titles[leg_titles.year.isin(dates)]
     print("Shorttitles:", shorttitles)
 
-    for fuzzy, method in zip([True, False], ('hybrid','exact')):
+    # for fuzzy, method in zip([True, False], ('hybrid','exact')):
+    for fuzzy, method in zip([True, False], ('exact','hybrid')):
         titles = shorttitles[shorttitles.for_fuzzy==fuzzy].candidate_titles.drop_duplicates().tolist()
         res = lookup_pipe(titles, doc, nlp, methods[method], conn, CUTOFF)
         print ("res:", res)
