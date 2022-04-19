f5184f9d707a9dc6861e018ddc70e73bd08133e0
niall.roche <niall.roche@gmail.com>
2022-04-19 12:09:09 +0100

    adding correct queue arn for abbreviations

--- modules/lambda_s3/lambda.tf
+++ modules/lambda_s3/lambda.tf
@@ -741,8 +741,8 @@ module "lambda-determine-replacements-abbreviations" {
 
   environment_variables = {
  
-    # DEST_QUEUE_NAME     = aws_sqs_queue.replacement-abbreviations-queue.url
-    DEST_QUEUE_NAME     = aws_sqs_queue.replacement-legislation-queue.url
+    DEST_QUEUE_NAME     = aws_sqs_queue.replacement-abbreviations-queue.url
+    # DEST_QUEUE_NAME     = aws_sqs_queue.replacement-legislation-queue.url
     # DEST_QUEUE_NAME     = aws_sqs_queue.replacement-abbreviations-queue.arn
     REPLACEMENTS_BUCKET = "${module.replacements_bucket.s3_bucket_id}"
     SOURCE_BUCKET = "${module.text_content_bucket.s3_bucket_arn}"
