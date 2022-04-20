1b3ec13c1f382b1fef9124d9079b4dff434175f6
niall.roche <niall.roche@gmail.com>
2022-04-20 18:38:57 +0100

    tweaking sns payload

--- lambda/xml_validate/index.py
+++ lambda/xml_validate/index.py
@@ -132,6 +132,7 @@ def handler(event, context):
         response = sns_client.publish (
         TargetArn = topic,
         
-        Message = json.dumps({'default': {'error': not valid_content, 'key': source_key, 'status': message}}),
+        # Message = json.dumps({'default': {'error': not valid_content, 'key': source_key, 'status': message}}),
+        Message = json.dumps({'default': message}),
         MessageStructure = 'json'
    )
