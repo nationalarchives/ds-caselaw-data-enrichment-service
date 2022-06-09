b958f8fbb8dfe3ee37acbd8ecddf31406d329ee7
enemsic <94066698+enemsic@users.noreply.github.com>
2022-06-09 12:02:49 +0100

    Add sparql in variables.tf

--- modules/lambda_s3/variables.tf
+++ modules/lambda_s3/variables.tf
@@ -77,6 +77,14 @@ variable "postgress_hostname" {
   # default = "tna-metadata-db-ucldetf-1.cvbrurw4plvi.eu-west-1.rds.amazonaws.com"
 }
 
+variable "sparql_username" {
+  type    = string
+}
+
+variable "sparql_password" {
+  type    = string
+}
+
 variable "vpc_id" {
   type    = string
 }
