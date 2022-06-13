369ed1354647c091c1348ca7644b186817a061e6
Amy Conroy <amyeileenconroy@gmail.com>
2022-06-13 17:56:00 +0100

    most of the replacement logic handled

--- legislation_provisions_extraction/legislation_provisions_v.0.2.py
+++ legislation_provisions_extraction/legislation_provisions_v.0.2.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 patterns = {
     'legislation':r'<ref(.*?)type=\"legislation\"(.*?)ref>',
     'section':r'( [sS]ection\W*[0-9]+(?=)|[sS]ections\W*[0-9]+| [sS]+\W*[0-9]+)(\W*\([0-9]+\))?', 
-    'sub_section': r'/\([0-9]+\)/gm' 
+    'sub_section': r'\([0-9]+\)' 
 }
 
 # get the href from the xml 
@@ -56,18 +56,14 @@ def find_closest_legislation(legislations, sections):
 
     return section_dict
 
-def generate_sub_section_links(section_dict): 
-    # we generate sub-section links here
-    a = 1
-
-
 def get_clean_section_number(section):
     section_number = re.findall(r'\d+', section)
     return section_number[0]
 
-def save_section_to_dict(section_dict, para_number, clean_section_dict): 
+def save_section_to_dict(section_dict, para_number, clean_section_dict):
 
     for section in section_dict: 
+
         section_number = get_clean_section_number(section)
         full_ref = section_dict[section]
         soup = BeautifulSoup(full_ref,'xml')
@@ -77,20 +73,92 @@ def save_section_to_dict(section_dict, para_number, clean_section_dict):
         canonical = ref.find('canonical')
         clean_section = "section " + str(section_number)
 
+        new_definition = {'para_number': para_number, 'ref': ref, 'leg_href': leg_href, 'canonical': canonical, 'section_href': section_href}
+
         if clean_section not in clean_section_dict.keys():
-            clean_section_dict[clean_section] = [{'para_number': para_number, 'ref': ref, 'leg_href': leg_href, 'canonical': canonical, 'section_href': section_href}]
+            clean_section_dict[clean_section] = [new_definition]
 
         else:
             value = clean_section_dict[clean_section]
-            new_value = [value, {'para_number': para_number, 'ref': ref, 'leg_href': leg_href, 'canonical': canonical, 'section_href': section_href}]
-            clean_section_dict[clean_section] = new_value
-      
+            value.append(new_definition)
+            clean_section_dict[clean_section] = value
+
+    
     return clean_section_dict
 
-def provision_replacer(text, section_dict):
+def create_sub_section_links(section_dict, match): 
 
+    print(section_dict)
+    curr_href = section_dict['section_href']
+    sub_section = re.findall(patterns['sub_section'], match)
+    sub_section_number = get_clean_section_number(sub_section[0])
+    new_href = curr_href + "/" + str(sub_section_number)
+    # replace href in section_dict with new_href
+    section_dict['section_href'] = new_href
+
+    return section_dict
+
+def check_if_sub_section(section):
+    if re.search(patterns['sub_section'], section):
+        return True
+    else:
+        return False
+
+def create_replacement_dictionary(section_dict, para_number, match, sub_section, clean_section):
+    return 
+
+def get_correct_section_def(section_matches, para_number): 
+
+    print(section_matches)
+
+    i = 0
+    print(len(section_matches))
+    while i < len(section_matches) - 1:
+        print(section_matches[i])
+        # if the current para number is greater than but less than next para number, return the current match
+        curr_match = section_matches[i]['para_number']
+        next_match = section_matches[i+1]['para_number']
+        if para_number > curr_match and para_number < next_match:
+            return section_matches[i]
+        
+        i += 1
+
+    return False
+
+def provision_replacer(text, section_dict, matches, para_number):
+    print("\n")
+    print(matches)
+    for match in matches:
+
+        clean_section_num = get_clean_section_number(match)
+        clean_section = "section " + str(clean_section_num)
+        if clean_section in section_dict.keys():
+            values = section_dict[clean_section]
+            print(values) 
+            # if they referred to the section before it was defined in a paragraph with linked leg, skip
+            if para_number < values[0]['para_number']:
+                continue
+            
+            # if the section was re-defined, handle this
+            if len(values) > 1: 
+                correct_reference = get_correct_section_def(values, para_number)
+                if correct_reference == False: 
+                    continue
+            
+            else: 
+                correct_reference = values[0]
+            
+            # check if this was a sub_section 
+            sub_section = check_if_sub_section(match)
+            print(correct_reference)
+            # TODO - change this so that it doesn't permanently change the sub-link
+            if sub_section:
+                correct_reference = create_sub_section_links(correct_reference, match)
+                
 
     return
+
+
 
 def main(enriched_judgment_file_path): 
     for filename in os.listdir(enriched_judgment_file_path):
@@ -107,35 +175,22 @@ def main(enriched_judgment_file_path):
                 legislations = detect_reference(str(line))
                 sections = detect_reference(str(line), 'section')
                 section_to_leg_matches = find_closest_legislation(legislations, sections)
-                # create the master section dictionary 
+                # create the master section dictionary with relevant leg links
                 section_dict = save_section_to_dict(section_to_leg_matches, para_number, section_dict)
-                print(section_dict)
+                
+                
+        # replacement logic
+        if section_dict:
+            para_number = 0
+            for line in text:
+                para_number += 1
+                matches = re.finditer(patterns['section'], str(line))
+                matches_lst = [i.group(0) for i in matches]
+                if matches_lst:
+                    provision_replacer(text, section_dict, matches_lst, para_number)
 
 
-
-
-
-
-
-
-
-
-
-        #         if sections and len(split_sentences) > 1:
-                #TODO: rewrite this according to how resolve_section_to_leg is implemented
-        #         for section in section_to_leg:
-        #             href = get_legislation_href(split_sentences) # update this to handle multiple refs in one line
-        #             section_href = create_section_href(section, href) 
-        #             new_section_dict = add_section_to_dict(section_dict, line_number, section, section_href) # maintaining the line number for the replacements
-        #             section_dict.update(new_section_dict)
-        # for k, v in section_dict.items():
-        #     print(k, v)
-        # print("\n")
 
 
 main("legislation_provisions_extraction/test_judgments")
-
-# TODO: 
-# - add support for sections referenced w/o legislation 
-# - switch to dictionary - looking at orphan provisions]
 
