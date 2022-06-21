8bd58f5e2024859cccbc058bd19bb07143d8ed38
Amy Conroy <amyeileenconroy@gmail.com>
2022-06-21 16:55:46 +0100

    formatting

--- oblique_references/oblique_references.py
+++ oblique_references/oblique_references.py
@@ -23,10 +23,12 @@ patterns = {
 
 def detect_reference(text, etype):
     references = [(m.span(), m.group()) for m in re.finditer(patterns[etype], text)]
+
     return references
 
 def create_legislation_dict(leg_references):
     legislation_dicts = []
+
     for leg_ref in leg_references:
         legislation_dict = {}
         soup = BeautifulSoup(leg_ref[1], 'xml')
@@ -35,37 +37,43 @@ def create_legislation_dict(leg_references):
         leg_year = re.search('\d{4}', leg_name).group(0)
         href = ref['href']
         canonical = ref['canonical']
-        legislation_dict["pos"] = leg_ref[0]
+        legislation_dict['pos'] = leg_ref[0]
         legislation_dict['detected_leg'] = leg_name
-        legislation_dict["href"] = href
-        legislation_dict["canonical"] = canonical
+        legislation_dict['href'] = href
+        legislation_dict['canonical'] = canonical
         legislation_dict['year'] = leg_year
         legislation_dicts.append(legislation_dict)
+
     return legislation_dicts
 
 def match_numbered_act(detected_numbered_act, legislation_dicts):
     act_year = re.search('\d{4}', detected_numbered_act[1]).group(0)
     for leg_dict in legislation_dicts:
         if leg_dict['year'] == act_year:
+
             return leg_dict
 
 def match_act(detected_act, legislation_dicts):
+    
     eligble_references = []
     act_pos = detected_act[0][0]
     for leg_dict in legislation_dicts:
         if leg_dict['pos'][0] < act_pos:
             eligble_references.append(leg_dict)
+
     positions = []
     for eligble_ref in eligble_references:
         positions.append(eligble_ref['pos'][0])
     correct_pos = max(positions)
     correct_ref = [ref for ref in eligble_references if ref['pos'][0] == correct_pos][0]
+
     return correct_ref
 
 def create_section_ref_tag(replacement_dict, match):
     canonical = replacement_dict['canonical']
     href = replacement_dict['href']
     oblique_ref = f'<ref href="{href}" uk:canonical="{canonical}" uk:type="legislation">{match.strip()}</ref>'
+
     return oblique_ref
 
 def get_replacements(detected_acts, legislation_dicts, numbered_act, replacements): 
