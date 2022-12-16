cae9947ba6751416ccb22221f83a993868726c22
enemsic <94066698+enemsic@users.noreply.github.com>
2022-12-16 12:23:40 +0000

    Enable make replacements lambda

--- modules/lambda_s3/queue.tf
+++ modules/lambda_s3/queue.tf
@@ -1,15 +1,15 @@
 
-# resource "aws_sqs_queue" "replacement-caselaw-queue" {
-#   name                       = "${local.name}-${local.environment}-replacement-caselaw-event-notification-queue"
-#   delay_seconds              = 90
-#   max_message_size           = 2048
-#   visibility_timeout_seconds = 900
-#   message_retention_seconds  = 86400
-#   receive_wait_time_seconds  = 10
-#   sqs_managed_sse_enabled    = true
-#   redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.replacements-caselaw_dlq_queue.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "replacement-caselaw-queue" {
+  name                       = "${local.name}-${local.environment}-replacement-caselaw-event-notification-queue"
+  delay_seconds              = 90
+  max_message_size           = 2048
+  visibility_timeout_seconds = 900
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 10
+  sqs_managed_sse_enabled    = true
+  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.replacements-caselaw_dlq_queue.arn}\",\"maxReceiveCount\":4}"
 
-# }
+}
 
 # resource "aws_sqs_queue_policy" "replacement-caselaw-queue-policy" {
 #   queue_url = aws_sqs_queue.replacement-caselaw-queue.id
@@ -31,64 +31,64 @@
 # POLICY
 # }
 
-# resource "aws_sqs_queue" "replacements-caselaw_dlq_queue" {
-#   name                      = "${local.name}-${local.environment}-replacements-caselaw-dlq-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
-#   #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "replacements-caselaw_dlq_queue" {
+  name                      = "${local.name}-${local.environment}-replacements-caselaw-dlq-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+  #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 
 
-# resource "aws_sqs_queue" "replacements_dlq_queue" {
-#   name                      = "${local.name}-${local.environment}-replacements-dlq-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
-#   #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "replacements_dlq_queue" {
+  name                      = "${local.name}-${local.environment}-replacements-dlq-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+  #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue" "replacements-queue" {
-#   name                       = "${local.name}-${local.environment}-replacements-queue"
-#   delay_seconds              = 90
-#   max_message_size           = 2048
-#   visibility_timeout_seconds = 900
-#   message_retention_seconds  = 86400
-#   receive_wait_time_seconds  = 10
-#   sqs_managed_sse_enabled    = true
-#   redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.replacements_dlq_queue.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "replacements-queue" {
+  name                       = "${local.name}-${local.environment}-replacements-queue"
+  delay_seconds              = 90
+  max_message_size           = 2048
+  visibility_timeout_seconds = 900
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 10
+  sqs_managed_sse_enabled    = true
+  redrive_policy             = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.replacements_dlq_queue.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue_policy" "replacements-queue-policy" {
-#   queue_url = aws_sqs_queue.replacements-queue.id
-#   policy    = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": "*",
-#       "Action": "sqs:SendMessage",
-#       "Resource": "${aws_sqs_queue.replacements-queue.arn}",
-#       "Condition": {
-#         "ArnEquals": { "aws:SourceArn": "${module.lambda-make-replacements.lambda_function_arn}" }
-#       }
-#     }
-#   ]
-# }
-# POLICY
-# }
+resource "aws_sqs_queue_policy" "replacements-queue-policy" {
+  queue_url = aws_sqs_queue.replacements-queue.id
+  policy    = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.replacements-queue.arn}",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${module.lambda-make-replacements.lambda_function_arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
 
 
 
@@ -187,104 +187,104 @@
 
 
 
-# resource "aws_sqs_queue" "validation-queue" {
-#   name = "${local.name}-${local.environment}-validation-event-notification-queue"
+resource "aws_sqs_queue" "validation-queue" {
+  name = "${local.name}-${local.environment}-validation-event-notification-queue"
 
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 86400
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
-#   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.validation_dlq_queue.arn}\",\"maxReceiveCount\":4}"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.validation_dlq_queue.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue_policy" "validation-queue-policy" {
-#   queue_url = aws_sqs_queue.validation-queue.id
-#   policy    = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": "*",
-#       "Action": "sqs:SendMessage",
-#       "Resource": "${aws_sqs_queue.validation-queue.arn}",
-#       "Condition": {
-#         "ArnEquals": { "aws:SourceArn": "${module.lambda-validate-replacements.lambda_function_arn}" }
-#       }
-#     }
-#   ]
-# }
-# POLICY
-# }
+resource "aws_sqs_queue_policy" "validation-queue-policy" {
+  queue_url = aws_sqs_queue.validation-queue.id
+  policy    = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.validation-queue.arn}",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${module.lambda-validate-replacements.lambda_function_arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
 
-# resource "aws_sqs_queue" "validation_dlq_queue" {
-#   name                      = "${local.name}-${local.environment}-validation-dlq-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
+resource "aws_sqs_queue" "validation_dlq_queue" {
+  name                      = "${local.name}-${local.environment}-validation-dlq-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue" "validation_updates_queue" {
-#   name                      = "${local.name}-${local.environment}-validation-updates-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
+resource "aws_sqs_queue" "validation_updates_queue" {
+  name                      = "${local.name}-${local.environment}-validation-updates-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue" "validation_updates_error_queue" {
-#   name                      = "${local.name}-${local.environment}-validation-updates-error-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
+resource "aws_sqs_queue" "validation_updates_error_queue" {
+  name                      = "${local.name}-${local.environment}-validation-updates-error-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue" "validation_updates_dlq_queue" {
-#   name                      = "${local.name}-${local.environment}-validation-updates-dlq-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
-#   #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "validation_updates_dlq_queue" {
+  name                      = "${local.name}-${local.environment}-validation-updates-dlq-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+  #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# resource "aws_sqs_queue" "validation_updates_error_dlq_queue" {
-#   name                      = "${local.name}-${local.environment}-validation-updates-error-dlq-queue"
-#   delay_seconds             = 90
-#   max_message_size          = 2048
-#   message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
-#   receive_wait_time_seconds = 10
-#   sqs_managed_sse_enabled   = true
-#   #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+resource "aws_sqs_queue" "validation_updates_error_dlq_queue" {
+  name                      = "${local.name}-${local.environment}-validation-updates-error-dlq-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 1209600 #max is 2 weeks or 1209600 secs
+  receive_wait_time_seconds = 10
+  sqs_managed_sse_enabled   = true
+  #   redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
 
-# resource "aws_sns_topic" "validation_updates" {
-#   name = "validation-updates-topic"
-# }
+resource "aws_sns_topic" "validation_updates" {
+  name = "validation-updates-topic"
+}
 
-# resource "aws_sns_topic" "validation_updates_error" {
-#   name = "validation-updates-error-topic"
-# }
+resource "aws_sns_topic" "validation_updates_error" {
+  name = "validation-updates-error-topic"
+}
 
 # resource "aws_sns_topic" "rules_update_error" {
 #   name         = "rules-update-error-topic"
