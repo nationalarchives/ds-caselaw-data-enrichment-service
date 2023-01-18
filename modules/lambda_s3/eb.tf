ec0a89981c4e8da83bfd5307e0379fcac068471f
Adam King <adam.king@mdrx.tech>
2023-01-18 12:17:25 +0000

    using resources instead of module for eb
    Signed-off-by: Adam King <adam.king@mdrx.tech>

--- modules/lambda_s3/eb.tf
+++ modules/lambda_s3/eb.tf
@@ -21,3 +21,17 @@
 #    }
 #  }
 #}
+
+resource "aws_cloudwatch_event_rule" "db_backup" {
+  name                = "db-backup"
+  description         = "Trigger lambda to perform db snapshot"
+  is_enabled          = false # update last
+  schedule_expression = "cron(0 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "db_backup_lambda" {
+  #arn  = module.db_backup_lambda.lambda_function_arn
+  arn   = module.lambda-fetch-xml.lambda_function_arn
+  rule  = aws_cloudwatch_event_rule.db_backup.name
+  input = jsonencode({ "db-name" : "tna-metadata-db-${local.environment}-1" })
+}
