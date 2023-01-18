f469d0774ad8c4ed3b15c73ce6212c3dbc68d097
Adam King <102671802+ak-mdrxtech@users.noreply.github.com>
2023-01-18 13:47:24 +0000

    Na 115 lmb (#81)
    * added the db backup lambda
    
    Signed-off-by: Adam King <adam.king@mdrx.tech>
    
    * lmb module version
    
    Signed-off-by: Adam King <adam.king@mdrx.tech>
    
    * Added policies
    
    Signed-off-by: Adam King <adam.king@mdrx.tech>
    
    * cleaned up eb.tf and pointed the backup at the correct lambda
    
    Signed-off-by: Adam King <adam.king@mdrx.tech>
    
    Signed-off-by: Adam King <adam.king@mdrx.tech>

--- modules/lambda_s3/eb.tf
+++ modules/lambda_s3/eb.tf
@@ -1,27 +1,3 @@
-#module "eb_db_backup" {
-#  source  = "terraform-aws-modules/eventbridge/aws"
-#  version = "~> 0.1"
-#
-#  create_bus = false
-#
-#  rules = {
-#    backup = {
-#      description         = "Trigger lambda to perform db snapshot"
-#      enabled             = false
-#      schedule_expression = "cron(0 12 * * ? *)"
-#    }
-#  }
-#
-#  targets = {
-#    backup = {
-#      name  = "db-backup-lambda"
-#      #arn   = module.db_backup_lambda.lambda_function_arn
-#      arn   = module.lambda-fetch-xml.lambda_function_arn
-#      input = jsonencode({ "db-name" : "tna-metadata-db-${local.environment}-1" })
-#    }
-#  }
-#}
-
 resource "aws_cloudwatch_event_rule" "db_backup" {
   name                = "db-backup"
   description         = "Trigger lambda to perform db snapshot"
@@ -30,8 +6,7 @@ resource "aws_cloudwatch_event_rule" "db_backup" {
 }
 
 resource "aws_cloudwatch_event_target" "db_backup_lambda" {
-  #arn  = module.db_backup_lambda.lambda_function_arn
-  arn   = module.lambda-fetch-xml.lambda_function_arn
+  arn   = module.db_backup_lambda.lambda_function_arn
   rule  = aws_cloudwatch_event_rule.db_backup.name
   input = jsonencode({ "db-name" : "tna-metadata-db-${local.environment}-1" })
 }
