58f453b3f90cfb132676deda56e8fd015a4e4345
Adam King <adam.king@mdrx.tech>
2023-01-31 11:44:22 +0000

    added the current account to the policy too
    Signed-off-by: Adam King <adam.king@mdrx.tech>

--- modules/secure_bucket/data.tf
+++ modules/secure_bucket/data.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "vcite_kms_policy" {
   statement {
     sid = "AllowVCiteRole"
@@ -7,6 +9,23 @@ data "aws_iam_policy_document" "vcite_kms_policy" {
     principals {
       identifiers = [
         "arn:aws:iam::926041203935:role/vlex-vcite",
+      ]
+      type = "AWS"
+    }
+
+    actions = [
+      "kms:*",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid = "Enable IAM User Permissions"
+
+    effect = "Allow"
+
+    principals {
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
       ]
       type = "AWS"
     }
