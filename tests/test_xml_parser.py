bb58537312c6aa962f467e4d14ff1acc2eecdfbc
Anthony Hashemi <ant.hashemi@gmail.com>
2023-02-23 17:27:24 +0000

    Fix testXMLStrings by moving second context tag inside first paragraph tag

--- tests/test_xml_parser.py
+++ tests/test_xml_parser.py
@@ -56,14 +56,12 @@ class TestXmlParser(unittest.TestCase):
                 <content>\
                     <p style="margin-left:0.50in;margin-right:0.00in;text-indent:0.5in">The present case is different.  The Sellers had a single obligation to provide and maintain the capacity to deliver gas from the Reservoirs at 130% of the TRDQ, whatever that was at the time.  They had no discretion as to how much daily gas they had to deliver.  As already noted, and the distinction is a subtle one, the power to change an obligation is not a discretion as to how to perform it.  Accordingly, once the obligation is identified and the breach established, the only remaining question is what if any loss has flowed from the breach.  There is no uncertainty about definition of the loss, though its quantification may be complex, depending on the circumstances.   </p>\
                 </content>\
-                </paragraph>\
-                <paragraph>\
-                <num style="font-style:normal;font-weight:normal;text-decoration:none;vertical-align:baseline;font-family:\'Times New Roman\';font-size:12pt;color:#000000;background-color:auto;margin-left:0.50in">115.</num>\
                 <content>\
                     <p style="margin-left:0.50in;margin-right:0.00in;text-indent:0.5in">I would therefore also allow the cross-appeal.  I am conscious that we are differing from the judge not once but twice, but that is no reflection on his judgment, which dealt clearly and succinctly with three difficult issues.  It should also be noted that his undisturbed finding on the third issue (no implied term) has had a decisive effect on the outcome and that his decision and ours have by different routes arrived at the same destination.  </p>\
                 </content>'
 
         judgment_content_text = parse_file(text)
+
         assert (
             judgment_content_text
             == "The present case is different.  The Sellers had a single obligation to provide and maintain the capacity to deliver gas from the Reservoirs at 130% of the TRDQ, whatever that was at the time.  They had no discretion as to how much daily gas they had to deliver.  As already noted, and the distinction is a subtle one, the power to change an obligation is not a discretion as to how to perform it.  Accordingly, once the obligation is identified and the breach established, the only remaining question is what if any loss has flowed from the breach.  There is no uncertainty about definition of the loss, though its quantification may be complex, depending on the circumstances. I would therefore also allow the cross-appeal.  I am conscious that we are differing from the judge not once but twice, but that is no reflection on his judgment, which dealt clearly and succinctly with three difficult issues.  It should also be noted that his undisturbed finding on the third issue (no implied term) has had a decisive effect on the outcome and that his decision and ours have by different routes arrived at the same destination."
