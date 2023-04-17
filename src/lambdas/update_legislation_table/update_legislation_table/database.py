00ef4b9dd55176c32605980a687f56644d586e0f
Anthony Hashemi <ant.hashemi@gmail.com>
2023-04-17 10:50:03 +0100

    remove_duplicates as part of update_legislation_table lambda

---
+++ src/lambdas/update_legislation_table/update_legislation_table/database.py
@@ -0,0 +1,35 @@
+"""
+This module contains utility functions for working with databases using SQLAlchemy.
+"""
+
+from sqlalchemy import Connection, text
+
+
+def remove_duplicates(db_conn: Connection, table_name: str):
+    """
+    Removes duplicate rows from the specified table in the database.
+    Uses a SQL query to identify duplicate rows based on all columns, except for the
+    unique row identifier, and deletes all but one of the duplicate rows.
+    Parameters
+    ----------
+    db_conn: sqlalchemy.engine.Connection, required.
+        The SQLAlchemy database connection object.
+    table_name: str, required.
+        The name of the table to remove duplicates from.
+    Returns
+    -------
+    None
+    """
+    sql_string = f"""
+        DELETE FROM {table_name}
+        WHERE ctid IN (
+        SELECT ctid
+        FROM (
+            SELECT ctid, ROW_NUMBER() OVER(PARTITION BY {table_name}.*) AS rnum
+            FROM {table_name}
+        ) t
+        WHERE t.rnum > 1
+        );
+    """
+    db_conn.execute(text(sql_string))
+    db_conn.commit()
---
+++ src/lambdas/update_legislation_table/update_legislation_table/tests/test_remove_duplicates.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+from testing.postgresql import Postgresql
+from update_legislation_table.database import remove_duplicates
+
+
+def test_remove_duplicates():
+    """
+    Given the test table has duplicate rows
+    When the function to remove_duplicates is called
+    Then the number of rows in the table should be reduced
+        due to the removed duplicates
+    """
+    with Postgresql() as postgresql:
+        engine = create_engine(postgresql.url())
+        with engine.connect() as conn:
+            conn.execute(
+                text("CREATE TABLE test_table (name VARCHAR(255), age INTEGER)")
+            )
+            conn.execute(
+                text(
+                    "INSERT INTO test_table (name, age) VALUES ('John', 30), ('John', 30), ('Sarah', 25)"
+                )
+            )
+            conn.commit()
+
+            remove_duplicates(conn, "test_table")
+
+            result = conn.execute(text("SELECT COUNT(*) FROM test_table")).scalar()
+            assert result == 2
