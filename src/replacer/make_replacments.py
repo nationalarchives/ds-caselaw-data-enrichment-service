fb7a7f63d241187be4ea72d6cc010f0a1fabc88f
Anthony Hashemi <ant.hashemi@gmail.com>
2023-09-25 19:38:35 +0100

    Refactor make_replacements

--- src/replacer/make_replacments.py
+++ src/replacer/make_replacments.py
@@ -1,16 +1,18 @@
 import json
 import logging
 import re
-from typing import List
+from typing import Tuple
 
 from bs4 import BeautifulSoup
+
+from replacer.replacer_pipeline import replacer_pipeline
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.DEBUG)
 
 
 def make_post_header_replacements(
-    original_content: str, post_header_replacement_content: str
+    original_content: str, replacement_patterns: str
 ) -> str:
     """
     Replaces the content following a closing header tag in a legal document with new content.
@@ -20,71 +22,62 @@ def make_post_header_replacements(
     - This function assumes a specific structure of the legal document with closing header tags.
 
     Args:
-        original_content (str): The original content of the legal document.
-        post_header_replacement_content (str): The replacement content to insert after the header.
+        original_content (str): The original content of the legal document
+        replacement_patterns (str): The line separated replacement patterns
 
     Returns:
         str: The modified legal document content with the replacement applied.
     """
     cleaned_file_content = sanitize_judgment(original_content)
-    document_split_by_header = split_text_by_closing_header_tag(cleaned_file_content)
-
-    post_header_content = document_split_by_header[2]
-
-    replaced_post_header_content = replace_text_content(
-        post_header_content, post_header_replacement_content
+    pre_header, end_header_tag, post_header = split_text_by_closing_header_tag(
+        cleaned_file_content
     )
+
+    replaced_post_header_content = apply_replacements(post_header, replacement_patterns)
     LOGGER.info("Got post-header replacement text content")
 
-    document_split_by_header[2] = replaced_post_header_content
-    full_replaced_text_content = "".join(document_split_by_header)
+    full_replaced_text_content = (
+        pre_header + end_header_tag + replaced_post_header_content
+    )
 
     return full_replaced_text_content
 
 
-def replace_text_content(file_content, replacements_content):
+def apply_replacements(content: str, replacement_patterns: str) -> str:
     """
     Run the replacer pipeline to make replacements on caselaw, legislation and abbreviations
     """
-    replacements = []
 
-    replacement_tuples_case = []
-    replacement_tuples_leg = []
-    replacement_tuples_abb = []
+    case_replacement_patterns = []
+    leg_replacement_patterns = []
+    abb_replacement_patterns = []
 
-    tuple_file = replacements_content
-    LOGGER.info("tuple_file")
-    LOGGER.info("---lines--")
+    for replacement_pattern_json in replacement_patterns.splitlines():
+        LOGGER.debug(replacement_pattern_json)
+        replacement_pattern_dict = json.loads(replacement_pattern_json)
 
-    for line in tuple_file.splitlines():
-        LOGGER.debug(line)
-        replacements.append(json.loads(line))
+        replacement_type, replacement_pattern_list = list(
+            replacement_pattern_dict.items()
+        )[0]
+        replacement_pattern = tuple(replacement_pattern_list)
 
-    for replacement in replacements:
-        key, value = list(replacement.items())[0]
+        if replacement_type == "case":
+            case_replacement_patterns.append(replacement_pattern)
 
-        LOGGER.info("replacements")
-        if key == "case":
-            case_law_tuple = tuple(replacement["case"])
-            replacement_tuples_case.append(case_law_tuple)
+        elif replacement_type == "leg":
+            leg_replacement_patterns.append(replacement_pattern)
 
-        elif key == "leg":
-            leg_tuple = tuple(replacement["leg"])
-            replacement_tuples_leg.append(leg_tuple)
-
-        else:
-            abb_tuple = tuple(replacement["abb"])
-            replacement_tuples_abb.append(abb_tuple)
-
-    from replacer.replacer_pipeline import replacer_pipeline
+        elif replacement_type == "abb":
+            abb_replacement_patterns.append(replacement_pattern)
 
     file_data_enriched = replacer_pipeline(
-        file_content,
-        replacement_tuples_case,
-        replacement_tuples_leg,
-        replacement_tuples_abb,
+        content,
+        case_replacement_patterns,
+        leg_replacement_patterns,
+        abb_replacement_patterns,
     )
-    LOGGER.info("Judgment enriched")
+
+    LOGGER.info("Content enriched")
 
     return file_data_enriched
 
@@ -137,13 +130,14 @@ def _remove_legislation_references(file_content):
     return file_content
 
 
-def split_text_by_closing_header_tag(file_content: str) -> List[str]:
+def split_text_by_closing_header_tag(content: str) -> Tuple[str, str, str]:
     """
-    Split file_content into start, closing header tag and body
+    Split content into start, closing header tag and body
     to ensure replacements only occur in the body.
     """
     header_patterns = [r"</header>", r"<header/>"]
     for pattern in header_patterns:
-        if pattern in file_content:
-            return re.split(f"({pattern})", file_content)
-    return ["", "", file_content]
+        if pattern not in content:
+            continue
+        return content.partition(pattern)
+    return "", "", content
