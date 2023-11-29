56c889991232dccdadcc22c2eec4c3db5b7f3954
David McKee <david.mckee@dxw.com>
2023-11-29 10:32:24 +0000

    Support wrapUp tag
    Discussed at https://trello.com/c/r3czs1q5/1528-introducing-new-wrapup-xml-element

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -14,7 +14,7 @@ def parse_file(file_data):
     :returns judgement_content_text: all content elements in judgment
     """
     soup = BeautifulSoup.BeautifulSoup(str(file_data), "xml")
-    judgment_content = soup.find_all(re.compile(r"(content|intro)"))
+    judgment_content = soup.find_all(re.compile(r"(content|intro|wrapUp)"))
     judgment_content_text = " ".join(
         [content.text.strip() for content in judgment_content]
     )
