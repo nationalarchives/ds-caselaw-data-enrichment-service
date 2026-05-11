5ebe58b2b5053a49a5387b215754c2878e85da3c
Jamie Mudle <jamie.mudle@dxw.com>
2026-05-11 16:29:11 +0100

    fix(FCLC-1833): updated value in main.tf from 16.8 to 16.11 for the aurora_rds postgres version to solve the issue during the staging-prod deployment process whereby postgres in aurora_rds can't be downgraded to 16.8 from 16.11

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ module "data" {
 
   aurora_rds = {
     "main" = {
-      engine_version = "16.8"
+      engine_version = "16.11"
       instance_type  = "db.t3.medium"
       allowed_security_groups = [
         module.network.default_security_group_id,
