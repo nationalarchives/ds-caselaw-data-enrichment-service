c3eeace50dfd999e2906b8bdd4d4e10336730872
anthonyhashemi <ant.hashemi@gmail.com>
2026-06-17 10:11:48 +0100

    Utilise db_connection fixture directly in test_remove_duplicates rather than db_engine

--- src/lambdas/update_legislation_table/update_legislation_table/tests/test_remove_duplicates.py
+++ src/lambdas/update_legislation_table/update_legislation_table/tests/test_remove_duplicates.py
@@ -3,19 +3,18 @@ from sqlalchemy import text
 from ..database import remove_duplicates
 
 
-def test_remove_duplicates(db_engine):
+def test_remove_duplicates(db_connection):
     """
     Given the test table has duplicate rows
     When the function to remove_duplicates is called
     Then the number of rows in the table should be reduced
         due to the removed duplicates
     """
-    with db_engine.connect() as conn:
-        conn.execute(text("CREATE TABLE test_table (name VARCHAR(255), age INTEGER)"))
-        conn.execute(text("INSERT INTO test_table (name, age) VALUES ('John', 30), ('John', 30), ('Sarah', 25)"))
-        conn.commit()
+    db_connection.execute(text("CREATE TABLE test_table (name VARCHAR(255), age INTEGER)"))
+    db_connection.execute(text("INSERT INTO test_table (name, age) VALUES ('John', 30), ('John', 30), ('Sarah', 25)"))
+    db_connection.commit()
 
-        remove_duplicates(conn, "test_table")
+    remove_duplicates(db_connection, "test_table")
 
-        result = conn.execute(text("SELECT COUNT(*) FROM test_table")).scalar()
-        assert result == 2
+    result = db_connection.execute(text("SELECT COUNT(*) FROM test_table")).scalar()
+    assert result == 2
